18ca1ed9c83fcc051a18d355052c94df73f1b734
alexandre.l <lienardalexandre@live.fr>
2017-05-03 17:17:07 +0200

    ex 1à algo3 almost broke a sweat :)

--- public/js/config/routes.js
+++ public/js/config/routes.js
@@ -62,4 +62,55 @@ export default ['$stateProvider', '$urlRouterProvider', '$locationProvider', ($s
             }],
             template: "{{dist}}"
         })
+        .state('algo3', {
+            url: '/algo3',
+            controller: ['$scope', function($scope){
+                function battle(player1, player2) {
+                    let game1 = [];
+                    let game2 = [];
+                    let battle = 0;
+  
+                    for ( battle = 0; battle < player1.length && battle < player2.length; battle ++){
+                        let victory = fight(player1[battle], player2[battle]);
+                        if (victory === "both alive"){
+                            game1.push(player1[battle]);
+                            game2.push(player2[battle]);
+                        }else if (victory === "card1")
+                            game1.push(player1[battle]);
+                        else if (victory === "card2")
+                            game2.push(player2[battle]);
+                    }
+                    if (battle < player1.length)
+                        for(battle; battle < player1.length; battle++)
+                            game1.push(player1[battle]);
+                    else if (battle < player2.length)
+                        for(battle; battle < player2.length; battle++)
+                            game2.push(player2[battle]);
+                    return {"player1": game1, "player2": game2};
+                }
+
+                function fight (card1, card2){
+                    card1[1] -= card2[0];
+                    card2[1] -= card1[0];
+  
+                    if (card1[1] > 0 && card2[1] > 0){
+                        card1[1] += card2[0];
+                        card2[1] += card1[0];
+                        return("both alive");
+                    }else if (card1[1] > 0){
+                        card1[1] += card2[0];
+                        card2[1] += card1[0];
+                        return ("card1")
+                    }else if (card2[1] > 0){
+                        card2[1] += card1[0];
+                        card1[1] += card2[0];
+                        return "card2";
+                    }
+                    card2[1] += card1[0];
+                    card1[1] += card2[0];
+                }
+                $scope.myGame = battle([ [ 1, 3 ], [ 3, 4 ] ], [ [ 2, 8 ], [5, 2] ]);
+            }],
+            template: "{{myGame}}"
+        })
 }]
